dbd870af071255f631efb0fbc4ef861b61907248
CryptoPascal31 <95504829+CryptoPascal31@users.noreply.github.com>
2026-05-04 08:23:09 +0000

    Updates

---
+++ systemd/start_systemd_relayer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export CONFIG_FILES="config/kda_hyperlane.json,config/rpcs.json,/etc/hyperlane/rpcs.json,config/relayer.json,$CREDENTIALS_DIRECTORY/relayer_secret.json"
+
+mkdir -p ~/.db/relayer
+
+AWS_ACCESS_KEY_ID="$(jq -r '.id' < "$CREDENTIALS_DIRECTORY/aws_secret")" \
+AWS_SECRET_ACCESS_KEY="$(jq -r '.key' < "$CREDENTIALS_DIRECTORY/aws_secret")" \
+exec ./agents/relayer
